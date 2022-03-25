2d2abfe1fa505933a49a469d1495e67e615f0a65
0bealan0 <beatriz.valenzuela.ajenjo@gmail.com>
2022-03-25 12:01:24 +0100

    tarea style win

--- code/script.js
+++ code/script.js
@@ -8,18 +8,40 @@ const btnResetea = document.querySelector('.btn--new');
 const scoreDos = document.getElementById('score--1');
 const currentDos = document.getElementById('current--1');
 const dado = document.querySelector('.dice');
+const playerUno = document.querySelector('.player--0');
+const playerDos = document.querySelector('.player--1');
+let current, scores, activePlayer;
+
 console.log(scoreUno);
 // const scoreTexts = document.querySelectorAll('.score');
 // for (const scoreText of scoreTexts) scoreText.textContent = '';
 
-scoreUno.textContent = 0;
-scoreDos.textContent = 0;
-
+const init = function () {
+  scores = [0, 0];
+  current = 0;
+  activePlayer = 0;
+  // scoreUno.textContent = 0;
+  // scoreDos.textContent = 0;
+  // currentUno.textContent = 0;
+  // currentDos.textContent = 0;
+  dado.classList.add('hidden');
+  for (const elemDOM of [currentDos, currentUno, scoreDos, scoreUno])
+    elemDOM.textContent = 0;
+};
+// function cambiar de jugador
+const switchPlayer = function () {
+  current = 0;
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer ? 0 : 1;
+  playerUno.classList.toggle('player--active');
+  playerDos.classList.toggle('player--active');
+};
+init();
 // Evento inicia
-
 btnInicia.addEventListener('click', function () {
   const numero = Math.trunc(Math.random() * 6 + 1);
-  currentUno.textContent = numero;
+  // dado.src=`dice-${numero}`
+  dado.classList.remove('hidden');
   switch (numero) {
     case 1:
       dado.src = 'dice-1.png';
@@ -42,12 +64,28 @@ btnInicia.addEventListener('click', function () {
   }
   if (numero === 1) {
     // llamo a la funcion cambiar de jugador
+    switchPlayer();
+  } else {
+    current += numero;
+    document.querySelector(`#current--${activePlayer}`).textContent = current;
   }
 });
 
-// function cambiar de jugador
-
 // Evento puntua
 btnPuntua.addEventListener('click', function () {
-  currentUno.textContent = scoreUno;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    Number(document.querySelector(`#score--${activePlayer}`).textContent) +
+    current;
+
+  if (`score--${activePlayer}` >= 100) {
+    playerUno.classList.toggle('player--winner');
+    playerDos.classList.toggle('player--winner');
+  } else {
+    switchPlayer();
+  }
+});
+
+// evento resetea
+btnResetea.addEventListener('click', function () {
+  init();
 });
