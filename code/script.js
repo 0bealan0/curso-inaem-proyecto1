1677a81e5cd28afa9ba143b921ea77566bc80d6a
0bealan0 <beatriz.valenzuela.ajenjo@gmail.com>
2022-03-25 14:26:24 +0100

    final

--- code/script.js
+++ code/script.js
@@ -10,7 +10,8 @@ const currentDos = document.getElementById('current--1');
 const dado = document.querySelector('.dice');
 const playerUno = document.querySelector('.player--0');
 const playerDos = document.querySelector('.player--1');
-let current, scores, activePlayer;
+let current, scores, activePlayer, playing;
+playing = true;
 
 console.log(scoreUno);
 // const scoreTexts = document.querySelectorAll('.score');
@@ -27,6 +28,12 @@ const init = function () {
   dado.classList.add('hidden');
   for (const elemDOM of [currentDos, currentUno, scoreDos, scoreUno])
     elemDOM.textContent = 0;
+  for (const playerEl of [playerUno, playerDos])
+    playerEl.classList.remove('player--winner');
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
 };
 // function cambiar de jugador
 const switchPlayer = function () {
@@ -39,53 +46,64 @@ const switchPlayer = function () {
 init();
 // Evento inicia
 btnInicia.addEventListener('click', function () {
-  const numero = Math.trunc(Math.random() * 6 + 1);
-  // dado.src=`dice-${numero}`
-  dado.classList.remove('hidden');
-  switch (numero) {
-    case 1:
-      dado.src = 'dice-1.png';
-      break;
-    case 2:
-      dado.src = 'dice-2.png';
-      break;
-    case 3:
-      dado.src = 'dice-3.png';
-      break;
-    case 4:
-      dado.src = 'dice-4.png';
-      break;
-    case 5:
-      dado.src = 'dice-5.png';
-      break;
-    case 6:
-      dado.src = 'dice-6.png';
-      break;
-  }
-  if (numero === 1) {
-    // llamo a la funcion cambiar de jugador
-    switchPlayer();
-  } else {
-    current += numero;
-    document.querySelector(`#current--${activePlayer}`).textContent = current;
+  if (playing) {
+    const numero = Math.trunc(Math.random() * 6 + 1);
+    // dado.src=`dice-${numero}`
+    dado.classList.remove('hidden');
+    switch (numero) {
+      case 1:
+        dado.src = 'dice-1.png';
+        break;
+      case 2:
+        dado.src = 'dice-2.png';
+        break;
+      case 3:
+        dado.src = 'dice-3.png';
+        break;
+      case 4:
+        dado.src = 'dice-4.png';
+        break;
+      case 5:
+        dado.src = 'dice-5.png';
+        break;
+      case 6:
+        dado.src = 'dice-6.png';
+        break;
+    }
+    if (numero === 1) {
+      // llamo a la funcion cambiar de jugador
+      switchPlayer();
+    } else {
+      current += numero;
+      document.querySelector(`#current--${activePlayer}`).textContent = current;
+    }
   }
 });
 
 // Evento puntua
 btnPuntua.addEventListener('click', function () {
-  document.querySelector(`#score--${activePlayer}`).textContent =
-    Number(document.querySelector(`#score--${activePlayer}`).textContent) +
-    current;
+  if (playing) {
+    // sumo las variables
+    scores[activePlayer] += current;
+    // lo mando al dom
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // document.querySelector(`#score--${activePlayer}`).textContent =
+    //   Number(document.querySelector(`#score--${activePlayer}`).textContent) +
+    //   current;
 
-  if (`score--${activePlayer}` >= 100) {
-    playerUno.classList.toggle('player--winner');
-    playerDos.classList.toggle('player--winner');
-  } else {
-    switchPlayer();
+    if (scores[activePlayer] >= 10) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      playing = false;
+    } else {
+      switchPlayer();
+    }
   }
 });
-
 // evento resetea
 btnResetea.addEventListener('click', function () {
+  dado.classList.remove('hidden');
   init();
 });
